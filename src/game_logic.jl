e2b84da7e1eeaf45986844a964aee3d2938cfe03
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-07-12 09:54:32 +0530

    updated game logic

--- src/game_logic.jl
+++ src/game_logic.jl
@@ -14,71 +14,111 @@ e     |
       |
 y     |
       V
+
 =#
+include("data_structures.jl")
+include("utils.jl")
+include("physics.jl")
 
-mutable struct Vector_
-    x::Float32
-    y::Float32
+function reset_ball!(arena::Arena, ball::Ball)
+    k = -1 * sign(ball.velocity.x)
+    ball.velocity.x = 2k
+    while true
+        ball.velocity.y = -1 * k * rand(-2:2)
+        ball.velocity.y != 0 && break
+    end
+    return arena.dims/2f0
 end
 
-Base.:+(a::Vector_, b::Vector_) = Vector_(a.x + b.x, a.y + b.y)
-Base.:*(a::Vector_, b::Vector_) = Vector_(a.x * b*x, a.y * b.y)
-Base.:*(k::Real, a::Vector_) = Vector_(k*a.x, k*a.y)
-Base.:*(a::Vector_, k::Real) = *(k, a)
+human_action!(player_b::Player, arena::Arena, dir) = update_paddle_position!(player_b, arena, dir)
 
-mutable struct Player
-    length::Float32
-    position::Vector_
-    velocity::Float32
-    is_left::Bool
-    Score::Int32
+function ai_action!(player_a::Player, arena::Arena, ball::Ball)
+    dir = sign(ball.position.y - player_a.length/2f0 - player_a.position.y)
+
+    if sign(ball.position.x - player_a.position.x) == sign(ball.velocity.x) ||
+        (player_a.position.y ≤ ball.position.y ≤ player_a.position.y + player_a.length)
+        dir = 0
+    end
+
+    update_paddle_position!(player_a, arena, dir)
 end
 
-mutable struct Ball
-    position::Vector_
-    velocity::Vector_
-    radius::Float32
+"""The default game configuration"""
+function Env()
+    arena_width = 80f0
+    arena_height = 80f0
+    paddle_length = arena_height/5
+
+    margin_height = floor(arena_height/30f0)
+    margin_width = floor(arena_width/30f0)
+
+    ball_radius = floor((margin_width + margin_height)/3f0)
+
+    arena = Arena(Vector_(arena_width, arena_height), Vector_(margin_width, margin_height))
+
+    # The position of the paddle is calculated from its center.
+    # Therefore, acceptable values of the y coordinate of the paddles are
+    # from 0 -------> arena_height - paddle_length
+    player_a_pos = Vector_(0, (arena.dims.y-paddle_length)/2)
+    player_a = Player(paddle_length, deepcopy(player_a_pos), 2, 0)
+
+    player_b_pos = Vector_(arena.dims.x, (arena.dims.y-paddle_length)/2)
+    player_b = Player(paddle_length, deepcopy(player_b_pos), 2, 0)
+
+    # Position of the ball is calculated from its center.
+    # Acceptable values of the x and y coordinates for the ball are from
+    # ball.radius -----------> arena_width - ball.radius
+    # ball.radius -----------> arena_height - ball.radius
+    ball_pos = Vector_(arena.dims.x/2f0, ball_radius)
+    ball_vel = Vector_(3,  -3)
+    ball = Ball(deepcopy(ball_pos), deepcopy(ball_vel), ball_radius)
+
+
+    return Env(arena, player_a, player_b, ball)
 end
 
-mutable struct Arena
-    dims::Vector_
-    margin::Vector_
-end
+function step!(env::Env, player_action)
+    #@assert player_action ∈ action_space
 
-mutable struct Env
-    arena::Arena
-    player_a::Player
-    player_b::Player
-    ball::Ball
-end
+    # 1 = Noop
+    # 2 = Up
+    # 3 = Down
+    dir = player_action == 1 ? 0 : (player_action == 2 ? -1 : 1)
 
-rand_x_y = (x, y) -> abs(x-y) * rand(Float32) + min(x, y)  # Generates a random number between x and y
-generate_random_height = (paddle_length, arena::Arena) -> rand_x_y(paddle_length/2f0, arena.dims.y - paddle_length/2f0)
-generate_random_ball_position(radius, arena::Arena) = Vector_(rand_x_y(radius, arena.dims.x-radius),
-                                                                rand_x_y(radius, arena.dims.y-radius))
+    arena = env.arena
+    player_a = env.player_a
+    player_b = env.player_b
+    ball = env.ball
 
-# Collisions between ball and the walls
-detect_collision(ball::Ball, arena::Arena) = (ball.position.y >= arena.dims.y-ball.radius-ball.velocity.y && ball.velocity.y > 0) ||
-                                (ball.position.y <= ball.radius+abs(ball.velocity.y)  && ball.velocity.y < 0)
+    reward = 0
 
-# Collisions between ball and the paddle
-function detect_collision(player::Player, ball::Ball, arena::Arena)
-    # The ball must be near one of the edges. Using Ball's velocity as an error term.
-    ball.radius-ball.velocity.x/2f0 < ball.position.x < (arena.dims.x-ball.radius-ball.velocity.x/2f0) && (return false)
+    total_score = player_a.score + player_b.score
 
-    lower_bound = max(player.position.y - player.length/2f0 - ball.radius/2f0, 0)
-    higher_bound = min(player.position.y + player.length/2f0 + ball.radius/2f0, arena.dims.x)
+    human_action!(player_b, arena, dir)
+    ai_action!(player_a, arena, ball)
+    new_position = env.ball.position + ball.velocity
 
-    # The ball must be at the same level as the paddle and it must be approaching the paddle.
-    lower_bound < ball.position.y < higher_bound && player.is_left == (ball.velocity.x<0) && (return true)
-end
+    wall_collision!(new_position, ball, arena)
+    paddle_collision!(new_position, player_a, player_b, ball, arena)
 
-rebound!(ball::Ball) = ball.velocity.y *= -1                 # Ball hits the walls
-rebound!(ball::Ball, player::Player) = ball.velocity.x *= -1 # Ball hits the paddle
+    if new_position.x ≤ 0
+        player_b.score += 1
+        reward = 1
+        println("Score!!!")
+        new_position = reset_ball!(arena, ball)
+    end
 
+    if new_position.x ≥ arena.dims.x
+        player_a.score += 1
+        reward = -1
+        println("Booo!!!!!")
+        new_position = reset_ball!(arena, ball)
+    end
 
-reset_ball!(ball::Ball, arena::Arena) = reset_ball!(ball, Vector_(arena.dims.x/2f0, arena.dims.y/2f0), Vector_(1,  -1))
-function reset_ball!(ball::Ball, position::Vector_, velocity::Vector_)
-    ball.position = position
-    ball.velocity = velocity
+    ball.position.x = new_position.x
+    ball.position.y = new_position.y
+
+    done = (player_a.score + player_b.score) ≥ 21
+
+    return get_obs(env), reward, done, Dict()
 end
